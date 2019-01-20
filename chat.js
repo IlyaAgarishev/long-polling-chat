9e1b3f5bd9e3814ec6ef0ae5f665c2190a7a1685
IlyaAgarishev <ilyagarishev@gmail.com>
2019-01-20 16:37:11 +0500

    first

---
+++ chat.js
@@ -0,0 +1,19 @@
+var clients = [];
+
+exports.subscribe = function(req, res) {
+  console.log('subscribe');
+  clients.push(res);
+
+  res.on('close', function() {
+    clients.splice(clients.indexOf(res), 1);
+  });
+};
+
+exports.publish = function(message) {
+  console.log("publish '%s'", message);
+  clients.forEach(function(res) {
+    res.end(`${message}secretSplitter${clients.length}`);
+  });
+
+  clients = [];
+};
